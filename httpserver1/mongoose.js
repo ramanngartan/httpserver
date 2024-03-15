adf43f876ab8a0c947e3450358de3d2d1706c2fc
Raman <86141147+ramanngartan@users.noreply.github.com>
2024-03-14 21:25:34 -0400

    Update mongoose.js

--- httpserver1/mongoose.js
+++ httpserver1/mongoose.js
@@ -1,1 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://admin:sevenel11@cluster0.ib8izhj.mongodb.net/userappnew");
 
+const app = express;
+app.use(express.json());
+
+const User = mongoose.model("Users", {
+  name: String,
+  username: String,
+  password: String,
+});
+
+
+app.post("/signup", function (req, res) {
+
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    const existingUser= User.findOne({username: username});
+    if (existingUser){
+        return res.status(400).send("Username already exists.")
+    }
+
+    const user = new User({
+        name: name,
+        username: username,
+        password: password,
+      });
+
+      user.save();
+      res.json({
+        "msg":"User created successfully."
+      })
+
+})
